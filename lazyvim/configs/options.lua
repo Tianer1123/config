ee1e8d33d072508cdff2d6e2c94b5592e22634d1
Tianer1123 <q2719833@126.com>
2025-02-17 17:53:00 +0800

    Create options.lua

---
+++ lazyvim/configs/options.lua
@@ -0,0 +1,8 @@
+vim.g.expandtab = true
+vim.g.tabspace = 4
+vim.g.shiftwidth = 4
+
+vim.g.lazyvim_python_lsp = "basedpyright"
+vim.g.lazyvim_python_ruff = "ruff"
+-- vim.g.lazyvim_rust_lsp = "rust_analyzer"
+vim.g.lazyvim_rust_lsp = "bacon-ls" -- for rust diagnostics
