958696a62708c1430b153d7748e053a54fff2f6d
Tianer1123 <q2719833@126.com>
2025-02-18 14:21:47 +0800

    Create colorscheme.lua

---
+++ lazyvim/plugins/colorscheme.lua
@@ -0,0 +1,14 @@
+return {
+    {
+        "catppuccin/nvim",
+        name = "catppuccin",
+        priority = 1000,
+    },
+
+    {
+        "LazyVim/LazyVim",
+        opts = {
+            colorscheme = "catppuccin-mocha",
+        },
+    },
+}
